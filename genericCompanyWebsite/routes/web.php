5e1135c09f2c301b4f5ac2a44df3596bc1a5ab0d
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-08-18 17:13:28 -0300

    Update add mailbox page

--- genericCompanyWebsite/routes/web.php
+++ genericCompanyWebsite/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('content.dashboard');
 })->name('home');
+
+Route::get('/mailbox', function () {
+    return view('content.mailbox');
+})->name('mailbox');
