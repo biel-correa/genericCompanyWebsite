c2378ca14a88f36132c7a5a109d1cc34abb8d36f
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-08-20 11:42:20 -0300

    Update show requester in task detail

--- genericCompanyWebsite/app/Tasks.php
+++ genericCompanyWebsite/app/Tasks.php
@@ -9,4 +9,9 @@ class Tasks extends Model
     protected $fillable = [
         'name', 'description',
     ];
+
+    public function requester()
+    {
+        return $this->hasOne(User::class, 'id', 'requester_id');
+    }
 }
--- genericCompanyWebsite/resources/views/content/task/viewTask.blade.php
+++ genericCompanyWebsite/resources/views/content/task/viewTask.blade.php
@@ -10,5 +10,18 @@
             <p>{{$task->description}}</p>
         </div>
     </div>
+    <div class="panel">
+        <div class="panel-body">
+            <h1>Related</h1>
+            <ul>
+                <li>
+                    Requester Name:
+                    <a href="{{route('users.editUserById', ['id'=>$task->requester->id])}}">
+                    {{$task->requester->name}}
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </div>
 @endsection
