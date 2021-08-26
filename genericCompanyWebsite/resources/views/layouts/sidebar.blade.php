1b1c1105648a097b7e2e119448d9033144632aba
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-08-26 16:33:44 -0300

    Ident sidebar

--- genericCompanyWebsite/resources/views/layouts/sidebar.blade.php
+++ genericCompanyWebsite/resources/views/layouts/sidebar.blade.php
@@ -2,10 +2,28 @@
 
     <ul class="sidebar-panel nav flex-column">
         <li class="sidetitle">MAIN</li>
-        <li><a href="{{route('users.index')}}"><span class="icon color7"><i class="fa fa-users"></i></span>Users<span
-            class="label label-default">{{ App\User::all()->count() }}</span></a></li>
-        <li><a href="{{route('tasks.index')}}"><span class="icon color7"><i class="fa fa-briefcase"></i></span>Tasks<span
-                class="label label-default">{{ App\Tasks::all()->count() }}</span></a></li>
+        <li>
+            <a href="{{route('users.index')}}">
+                <span class="icon color7">
+                    <i class="fa fa-users"></i>
+                </span>
+                Users
+                <span class="label label-default">
+                    {{ App\User::all()->count() }}
+                </span>
+            </a>
+        </li>
+        <li>
+            <a href="{{route('tasks.index')}}">
+                <span class="icon color7">
+                    <i class="fa fa-briefcase"></i>
+                </span>
+                Tasks
+                <span class="label label-default">
+                    {{ App\Tasks::all()->count() }}
+                </span>
+            </a>
+        </li>
     </ul>
 
 </div>
