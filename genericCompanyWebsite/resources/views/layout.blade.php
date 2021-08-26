0274338c912c27ee0f8470720dbade9dca626051
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-08-26 11:16:39 -0300

    Fix responsive select2 form

--- genericCompanyWebsite/resources/views/layout.blade.php
+++ genericCompanyWebsite/resources/views/layout.blade.php
@@ -18,6 +18,10 @@
     .d-flex{
       display: flex;
     }
+
+    .select2-container{
+      width: 100% !important;
+    }
   </style>
   <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
