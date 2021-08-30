2a998401198cef63d7b554478c2cff475d32c3cb
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-08-30 17:43:28 -0300

    Update change landing page

---
+++ genericCompanyWebsite/resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('content')
+<div class="page-header">
+    <h1 class="title">Welcome to your admin</h1>
+</div>
+@endsection
--- genericCompanyWebsite/routes/web.php
+++ genericCompanyWebsite/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('users.index');
+    return view('home');
 })->name('home');
 
 Route::get('/mailbox', function () {
