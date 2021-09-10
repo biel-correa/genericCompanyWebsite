5d7efdf78cd343b5303843d943bc0388e88147f5
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-09-10 08:47:26 -0300

    Update tasks show view adding tabs

--- genericCompanyWebsite/resources/views/content/tasks/viewTask.blade.php
+++ genericCompanyWebsite/resources/views/content/tasks/viewTask.blade.php
@@ -10,49 +10,57 @@
         <a class="btn btn-primary" href="{{ route('tasks.edit', $data->id) }}"><i class="fa fa-pencil"></i> Edit</a>
     </div>
 </div>
-@if ($data->description)
-    <div class="panel">
-        <div class="panel-body">
-            <h1>Description</h1>
-            <p>{{$data->description}}</p>
-            @if ($data->expiration_date && strtotime($data->expiration_date) >= strtotime(date('d-m-Y')))
-                <h3>Needs to be delivered until {{date('d/m/Y', strtotime($data->expiration_date))}}</h3>
-            @elseif ($data->expiration_date && strtotime($data->expiration_date) < strtotime(date('d-m-Y')))
-                <h3>Should have been delivered {{date('d/m/Y', strtotime($data->expiration_date))}}</h3>
-            @endif
-        </div>
-    </div>
-@endif
-@if (!$data->description)
-    <div class="panel">
-        <div class="panel-body">
-            <div class="col-md-12 text-center">
-                <h1>There is no description for this task</h1>
-                <a class="btn btn-success" href="{{route('tasks.edit', ['id'=>$data->id])}}">Add one</a>
-            </div>
-            @if ($data->expiration_date)
-                <h3>Needs to be delivered until {{date('d/m/Y', strtotime($data->expiration_date))}}</h3>
-            @endif
-        </div>
-    </div>
-@endif
 <div class="panel">
     <div class="panel-body">
-        <h1>Related</h1>
-        <ul>
-            <li>
-                Requested by:
-                <a href="{{route('users.show', ['id'=>$data->requester->id])}}">
-                {{$data->requester->name}}
-                </a>
-            </li>
-            <li>
-                Assigned to:
-                <a href="{{route('users.show', ['id'=>$data->assignedTo->id])}}">
-                {{$data->assignedTo->name}}
-                </a>
-            </li>
-        </ul>
+        <div role="tabpanel">
+
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs" role="tablist">
+              <li role="presentation" class="active"><a href="#description" aria-controls="description" role="tab" data-toggle="tab">Description</a></li>
+              <li role="presentation"><a href="#related" aria-controls="related" role="tab" data-toggle="tab">Related</a></li>
+            </ul>
+
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <div role="tabpanel" class="tab-pane active" id="description">
+                    @if ($data->description)
+                        <h1>Description</h1>
+                        <p>{{$data->description}}</p>
+                        @if ($data->expiration_date && strtotime($data->expiration_date) >= strtotime(date('d-m-Y')))
+                            <h3>Needs to be delivered until {{date('d/m/Y', strtotime($data->expiration_date))}}</h3>
+                        @elseif ($data->expiration_date && strtotime($data->expiration_date) < strtotime(date('d-m-Y')))
+                            <h3>Should have been delivered {{date('d/m/Y', strtotime($data->expiration_date))}}</h3>
+                        @endif
+                    @else
+                        <div class="col-md-12 text-center">
+                            <h1>There is no description for this task</h1>
+                            <a class="btn btn-success" href="{{route('tasks.edit', ['id'=>$data->id])}}">Add one</a>
+                        </div>
+                        @if ($data->expiration_date)
+                            <h3>Needs to be delivered until {{date('d/m/Y', strtotime($data->expiration_date))}}</h3>
+                        @endif
+                    @endif
+                </div>
+                <div role="tabpanel" class="tab-pane" id="related">
+                    <h1>Related</h1>
+                    <ul>
+                        <li>
+                            Requested by:
+                            <a href="{{route('users.show', ['id'=>$data->requester->id])}}">
+                            {{$data->requester->name}}
+                            </a>
+                        </li>
+                        <li>
+                            Assigned to:
+                            <a href="{{route('users.show', ['id'=>$data->assignedTo->id])}}">
+                            {{$data->assignedTo->name}}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div> 
     </div>
 </div>
 @endsection
